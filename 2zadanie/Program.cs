e2ddf45083e7b785d266c803e16ae6cb4a6f15fd
DiasKapassov <diasgoose@gmail.com>
2023-03-28 19:44:01 +0600

    Сделал 2 задание

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("3-х значное число: ");
+int a = int.Parse(Console.ReadLine());
+ if (a < 100) {
+    Console.Write("Нету последнего");
+ }
+ else
+ { while (a >= 1000)
+   {
+   a /= 10;
+   }
+   Console.Write($"{a % 10}");
+ }
