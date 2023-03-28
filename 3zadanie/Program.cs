1d754cd0a65afc488e2889f7ab3e87af4fd05f5a
DiasKapassov <diasgoose@gmail.com>
2023-03-28 19:21:52 +0600

    Закончил 3 задание

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Впиши день: ");
+int a = int.Parse(Console.ReadLine());
+if ((a == 6) || (a == 7)) {
+    Console.WriteLine("Выходной");
+}
+else {
+    if ((a > 0) && (a < 6)) {
+        Console.WriteLine("Нет");
+    }
+else{
+    Console.WriteLine("Всего 7 дней");
+    }
+}
